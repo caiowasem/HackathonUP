cc7dfea2baaad6cdad0c432251078d6eff5f158e
caiowasem <caiowasem@gmail.com>
2019-11-25 22:46:08 -0300

    Update BeijingAirQuality.R

--- BeijingAirQuality.R
+++ BeijingAirQuality.R
@@ -3,7 +3,7 @@
 # CAIO CESAR WASEM
 # 25/11/2019
 
-install.packages("ggpubr") #Para teste de correlação
+install.packages("ggpubr") #Para teste de correlaÃ§Ã£o
 
 library(readr)
 library(plyr)
@@ -13,17 +13,17 @@ library(ggplot2)
 library(tidyverse)
 library(ggpubr)
 
-#Definir diretório com as tabelas
+#Definir diretÃ³rio com as tabelas
 setwd("F:\\Documents\\BeijingAirQuality")
 
 #Importar base de dados
 files <- list.files(pattern = "*.csv", full.names = TRUE)
 dataBeijing <- ldply(files, read_csv)
 
-#Verificar variáveis do dataframe
+#Verificar variÃ¡veis do dataframe
 str(dataBeijing)
 
-#Função para deletar NA
+#FunÃ§Ã£o para deletar NA
 delete.na <- function(DF, n=0) {
   DF[rowSums(is.na(DF)) <= n,]
 }
@@ -31,22 +31,7 @@ delete.na <- function(DF, n=0) {
 #Remover NA (De 420768 linhas para 382168)
 dataBeijing <- delete.na(dataBeijing)
 
-#Separar em níveis de perigo (n1 é o melhor, n6 é o pior)
-# pm25.n1 <- dataBeijing[which(dataBeijing$PM2.5<=30),]
-# pm25.n2 <- dataBeijing[which((dataBeijing$PM2.5>30)&(dataBeijing$PM2.5<=60)),]
-# pm25.n3 <- dataBeijing[which((dataBeijing$PM2.5>60)&(dataBeijing$PM2.5<=90)),]
-# pm25.n4 <- dataBeijing[which((dataBeijing$PM2.5>90)&(dataBeijing$PM2.5<120)),]
-# pm25.n5 <- dataBeijing[which((dataBeijing$PM2.5>120)&(dataBeijing$PM2.5<=250)),]
-# pm25.n6 <- dataBeijing[which(dataBeijing$PM2.5>250),]
-# 
-# pm10.n1 <- dataBeijing[which(dataBeijing$PM10<=50),]
-# pm10.n2 <- dataBeijing[which((dataBeijing$PM10>50)&(dataBeijing$PM10<=100)),]
-# pm10.n3 <- dataBeijing[which((dataBeijing$PM10>100)&(dataBeijing$PM10<=250)),]
-# pm10.n4 <- dataBeijing[which((dataBeijing$PM10>250)&(dataBeijing$PM10<=350)),]
-# pm10.n5 <- dataBeijing[which((dataBeijing$PM10>350)&(dataBeijing$PM10<=430)),]
-# pm10.n6 <- dataBeijing[which(dataBeijing$PM10>430),]
-
-#Criar colunas com o nível de perigo para cada particulado
+#Criar colunas com o nÃ­vel de perigo para cada particulado
 dataBeijing["AQI25"] <- NA
 dataBeijing$AQI25[dataBeijing$PM2.5<=30] <- 1
 dataBeijing$AQI25[(dataBeijing$PM2.5>30)&(dataBeijing$PM2.5<=60)] <-2
@@ -63,7 +48,7 @@ dataBeijing$AQI10[(dataBeijing$PM2.5>250)&(dataBeijing$PM2.5<=350)] <-4
 dataBeijing$AQI10[(dataBeijing$PM2.5>350)&(dataBeijing$PM2.5<=430)] <-5
 dataBeijing$AQI10[dataBeijing$PM2.5>430] <- 6
 
-#Transformar variáveis para factors
+#Transformar variÃ¡veis para factors
 dataBeijing$wd <- as.factor(dataBeijing$wd)
 dataBeijing$station <- as.factor(dataBeijing$station)
 dataBeijing$AQI10 <- as.factor(dataBeijing$AQI10)
@@ -101,7 +86,7 @@ ggscatter(filter(dataBeijing, dataBeijing$O3<450), x="O3", y="PM2.5", conf.int =
 ggscatter(filter(dataBeijing, dataBeijing$O3<450), x="O3", y="PM10", conf.int = TRUE,
           cor.coef = TRUE, cor.method = "pearson")
 
-#PM x Temperatura e Pressão
+#PM x Temperatura e PressÃ£o
 ggscatter(dataBeijing, x="TEMP", y="PM2.5", conf.int = TRUE,
           cor.coef = TRUE, cor.method = "pearson")
 
@@ -120,7 +105,7 @@ ggscatter(dataBeijing, x="DEWP", y="PM2.5", conf.int = TRUE,
 
 ggscatter(dataBeijing, x="DEWP", y="PM10", conf.int = TRUE,
           cor.coef = TRUE, cor.method = "pearson")
-#PM x Precipitação
+#PM x PrecipitaÃ§Ã£o
 ggscatter(filter(dataBeijing, dataBeijing$RAIN>0), x="RAIN", y="PM2.5", conf.int = TRUE,
           cor.coef = TRUE, cor.method = "pearson")
 
@@ -133,7 +118,7 @@ ggscatter(filter(dataBeijing, dataBeijing$WSPM<10), x="WSPM", y="PM2.5", conf.in
 ggscatter(filter(dataBeijing, dataBeijing$WSPM<10), x="WSPM", y="PM10", conf.int = TRUE,
           cor.coef = TRUE, cor.method = "pearson")
 
-#AQI x Direção do Vento
+#AQI x DireÃ§Ã£o do Vento
 ggplot(data = dataBeijing, mapping = aes(x = AQI10, y=..prop..,group=1)) + 
   geom_bar(position = "identity") +
   facet_wrap(~wd)
@@ -178,7 +163,7 @@ ggplot(data = dataBeijing)+
   geom_bar(mapping = aes(x=AQI10, y=..prop..,group=1))+
   facet_wrap(~station, nrow=3)
 
-#AQI x Mês
+#AQI x MÃªs
 ggplot(data = dataBeijing)+
   geom_bar(mapping = aes(x=AQI10, y=..prop..,group=1))+
   facet_wrap(~month, nrow=3)
@@ -187,7 +172,7 @@ ggplot(data = dataBeijing)+
   geom_bar(mapping = aes(x=AQI25, y=..prop..,group=1))+
   facet_wrap(~month, nrow=3)
 
-#Temperatura x Mês
+#Temperatura x MÃªs
 ggplot(data = dataBeijing) + 
   stat_summary(
     mapping = aes(x = month, y = TEMP),
